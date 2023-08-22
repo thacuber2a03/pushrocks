d5201c416771f4a6e242e80edebbc3badd31ac98
thacuber2a03 <antonidiaz200731@gmail.com>
2023-08-21 20:33:09 -0400

    feat(backend): fast but it's completely broken :)

--- src/main.lua
+++ src/main.lua
@@ -5,6 +5,7 @@ Screen = {
 	isDirty = true,
 	toChange = {},
 	pixels = {},
+	didChange = false,
 }
 
 GFX.setDrawOffset(
@@ -32,28 +33,21 @@ end
 ---Set all the pixels on the screen to `color`.
 ---@param color Color?
 function Screen.clear(color)
-	for y=0, SCREEN_SIZE-1 do
-		for x=0, SCREEN_SIZE-1 do
-			table.insert(Screen.toChange, { x=x, y=y, color = color or WHITE })
-		end
-	end
+	Screen.drawRect(0, 0, SCREEN_SIZE, SCREEN_SIZE, color or WHITE)
 end
 
 ---Set a pixel in the screen to `color`.
----@param x integer
----@param y integer
+---@param x number
+---@param y number
 ---@param color Color?
 function Screen.setPixel(x, y, color)
 	if x < 0 or y < 0 or x >= SCREEN_SIZE or y >= SCREEN_SIZE then return end
-	table.insert(Screen.toChange, {
-		x = math.floor(x),
-		y = math.floor(y),
-		color=color or BLACK
-	})
-	Screen.isDirty = true
+	if Screen.pixels[y+1][x+1] == color then return end
+	table.insert(Screen.toChange, { x=x, y=y, color=color or BLACK })
+	Screen.requestRedraw = true
 end
 
----Draw sprite onto screen.
+---Draw a sprite onto the screen.
 ---@param sprite Sprite
 ---@param x integer
 ---@param y integer
@@ -63,14 +57,42 @@ function Screen.drawSprite(sprite, x, y)
 			local pos = (py*sprite.width+px)+1
 			local char = sprite.data:sub(pos, pos)
 			local col
-			--TODO(thacuber2a03): a stupid `tonumber` seems to be enough
 			if     char == "0" then col = WHITE
 			elseif char == "1" then col = BLACK
 			elseif char == "2" then col = XOR
+			elseif char == "-" then goto continue
 			end
 			if not col then error("invalid color "..char) end
 			Screen.setPixel(x+px, y+py, col)
+		    ::continue::
 		end
+	end
+end
+
+---Draw a rectangle onto the screen.
+---@param x integer
+---@param y integer
+---@param w integer
+---@param h integer
+---@param color Color?
+function Screen.drawRect(x, y, w, h, color)
+	for ry=0, h-1 do
+		for rx=0, w-1 do
+			Screen.setPixel(x+rx, y+ry, color)
+		end
+	end
+end
+
+---`ipairs`, in reverse.
+---@param t table
+---@return function
+local function ripairs(t)
+	local i=#t+1
+	return function()
+		i=i-1
+		local v = t[i]
+		if not v then return end
+		return i, v
 	end
 end
 
@@ -90,32 +112,31 @@ function PD.update()
 		Screen.reset(WHITE)
 		init = false
 	else
-		local speed = .5
-		if PD.buttonIsPressed "right" then px = px + speed end
-		if PD.buttonIsPressed "left"  then px = px - speed end
-		if PD.buttonIsPressed "down"  then py = py + speed end
-		if PD.buttonIsPressed "up"    then py = py - speed end
+		local speed = 1 -- in pixels
+		if PD.buttonJustPressed "right" then px = px + speed end
+		if PD.buttonJustPressed "left"  then px = px - speed end
+		if PD.buttonJustPressed "down"  then py = py + speed end
+		if PD.buttonJustPressed "up"    then py = py - speed end
 
 		Screen.clear()
 		Screen.drawSprite(testSprite, px, py)
 
-		if Screen.isDirty then
-			for _, p in ipairs(Screen.toChange) do
+		if Screen.requestRedraw then
+			Screen.didChange = false
+			for _, p in ripairs(Screen.toChange) do
 				local x, y = p.x+1, p.y+1
-				if p.color ~= Screen.pixels[y][x] then
-					local pixelRealSize = SCALE -- real
-					GFX.setColor(p.color)
-					GFX.fillRect(
-						p.x*pixelRealSize,
-						p.y*pixelRealSize,
-						pixelRealSize, pixelRealSize
-					)
-					Screen.pixels[y][x] = p.color
-				end
+				local pixelRealSize = SCALE -- real
+				GFX.setColor(p.color)
+				GFX.fillRect(
+					p.x*pixelRealSize,
+					p.y*pixelRealSize,
+					pixelRealSize, pixelRealSize
+				)
+				Screen.pixels[y][x] = p.color
+				Screen.didChange = true
 			end
 			Screen.toChange = {}
-			collectgarbage "collect"
-			Screen.isDirty = false
+			Screen.requestRedraw = false
 		end
 	end
 end
